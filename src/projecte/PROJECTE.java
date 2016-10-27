73eedb231ff13cce795efb37de1379cb36e2aa6f
alumne <alumne@alumne-iesebre>
2016-10-27 09:50:57 +0200

    es el meu primer commit

--- src/projecte/PROJECTE.java
+++ src/projecte/PROJECTE.java
@@ -20,7 +20,7 @@ public class PROJECTE {
         // TODO code application logic here
         Scanner ent= new Scanner(System.in);
         String rasa;
-        int edat;
+        int germans;
         String color;
         boolean sexe;
         double pes;
@@ -34,11 +34,41 @@ public class PROJECTE {
             System.out.println("3.Modificar gos.");
             System.out.println("Llistar gos.");
            
-            System.out.println("Introdueix la raça:");
+            System.out.println("Introdueix la rasa:");
             rasa=ent.next();
             System.out.println("la raça és "+rasa);
-        } while (true);
-    
+            
+            switch (opcio) {
+                case 1:
+                    if(!omplit){
+                        System.out.println("Introdueix la rasa:");
+                        rasa=ent.nextLine();
+                        System.out.println("Quants germans que té?:");
+                        germans=ent.nextInt();
+                        System.out.println("De quin color es?:");
+                        color=ent.nextLine();
+                        System.out.println("Es mascle o femella?:");
+                        sexe=ent.nextBoolean();
+                        System.out.println("Quin es el seu pes?:");
+                        pes=ent.nextDouble();
+                        omplit=true
+                                
+                    
+                    
+                    }
+                    
+                    
+                    
+                    if(!omplit){
+                        System.out.println("");
+                    }
+                        
+                        
+                        
+                        break;
+                default:
+                    throw new AssertionError();
+            }
     
     
     
